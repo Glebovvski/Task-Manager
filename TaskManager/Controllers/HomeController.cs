fb62171cb872097369a178477fc6b2981d6fa97a
Gleb <zadachings@gmail.com>
2017-09-22 20:59:29 +0300

    Update HomeController.cs

--- TaskManager/Controllers/HomeController.cs
+++ TaskManager/Controllers/HomeController.cs
@@ -1,31 +1,64 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TaskManager.Models;
-using PagedList;
-using PagedList.Mvc;
+using System.IO;
 
 namespace TaskManager.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private TaskContext context;
+
+        public HomeController()
+        {
+            context = new TaskContext();
+        }
+
+        public ActionResult Index(int? page=1)
+        {
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+
+            var tasks = context.Task.OrderBy(c => c.Id).Take(pageSize);
+            return View(tasks);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public JsonResult LoadMoreTasks(int size)
+        {
+            var model = context.Task.OrderBy(p => p.Id).Skip(size).Take(3);
+            int modelCount = context.Task.Count();
+            if (model.Any())
+            {
+                string modelString = RenderRazorViewToString("_LoadMoreTasks", model);
+                return Json(new { ModelString = modelString, ModelCount = modelCount });
+            }
+            return Json(model);
+        }
+
+        public string RenderRazorViewToString(string viewName, object model)
+        {
+            ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                var viewContext =
+                     new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+
         public ActionResult TaskView(int id)
         {
-            var context = new TaskContext();
             var task = context.Task.Find(id);
             return View(task);
         }
-        public ActionResult Index(int? page)
-        {
-            var context = new TaskContext();
-            var tasks = context.Task.OrderByDescending(c => c.LastModified).ToList();
-            int pageSize = 3;
-            int pageNumber = (page ?? 1);
-            return View(tasks.ToPagedList(pageNumber,pageSize));
-        }
-
     }
 }
