767f431364ca568d0aff154615c0542634a79a9e
Gleb <zadachings@gmail.com>
2017-09-23 23:30:32 +0300

    Update PersonalTask.cs

--- TaskManager/Models/PersonalTask.cs
+++ TaskManager/Models/PersonalTask.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,12 +9,18 @@ namespace TaskManager.Models
 {
     public class PersonalTask
     {
+        public PersonalTask()
+        {
+            Tags = new List<string>();
+        }
         [Key]
         public int Id { get; set; }
         [Required]
+        public List<string> Tags { get; set; }
+        [Required]
         public string Title { get; set; }
         [Required]
-        [MinLength(20)]
+        [MinLength(20), MaxLength(100000)]
         public string Content { get; set; }
         public DateTime LastModified { get; set; }
     }
