675c5a677797c8eff0a0426210cd44db5c87986d
Gleb <zadachings@gmail.com>
2017-09-23 23:29:42 +0300

    Add files via upload

---
+++ TaskManager/Helpers/CreateTagList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Helpers
+{
+    public static class CreateTagList
+    {
+        public static List<string> TagsList(List<string> tags)
+        {
+            string foo = "";
+            foreach(var tag in tags)
+            {
+                foo=string.Concat(foo, tag);
+            }
+            List<string> tagsList = new List<string>();
+            foreach (var element in foo)
+            {
+                string tag = "";
+                while (element != ',' || element != ' ' || element != ';' || element != '.')
+                {
+                    tag = string.Concat(tag, element);
+                }
+                tagsList.Add(tag);
+            }
+            return tagsList;
+        }
+    }
+}
---
+++ TaskManager/Helpers/Utility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManager.Helpers
+{
+    public static class Utility
+    {
+        #region Batching, take a certain number of collection and return it each time through the loop
+        public static IEnumerable<IEnumerable<T>> Batch<T>(
+            this IEnumerable<T> source, int batchSize)
+        {
+            using (var enumerator = source.GetEnumerator())
+                while (enumerator.MoveNext())
+                    yield return YieldBatchElements(enumerator, batchSize - 1);
+        }
+
+        private static IEnumerable<T> YieldBatchElements<T>(
+            IEnumerator<T> source, int batchSize)
+        {
+            yield return source.Current;
+            for (int i = 0; i < batchSize && source.MoveNext(); i++)
+                yield return source.Current;
+        }
+        #endregion
+    }
+}
