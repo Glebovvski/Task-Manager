74b30d391422a1c2a11fe8774df66cded2217183
Gleb <zadachings@gmail.com>
2017-09-23 23:23:02 +0300

    Update EditApiController.cs

--- TaskManager/Controllers/EditApiController.cs
+++ TaskManager/Controllers/EditApiController.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using TaskManager.Helpers;
 using TaskManager.Models;
 
 namespace TaskManager.Controllers
@@ -10,24 +11,29 @@ namespace TaskManager.Controllers
     [Authorize]
     public class EditApiController : ApiController
     {
+        private TaskContext context;
+
+        public EditApiController()
+        {
+            context = new TaskContext();
+        }
         // GET: Update/Edit/5
         [HttpGet]
         public IHttpActionResult Get(int id)
         {
-            var context = new TaskContext();
             var task = context.Task.FirstOrDefault(x => x.Id == id);
             return Ok(task);
         }
 
         // POST: Update/Edit/5
         [HttpPatch,HttpPut]
-        public IHttpActionResult Patch(int id, [FromBody]string content)
+        public IHttpActionResult Patch(int id, [FromBody]string content, [FromBody]string title, [FromBody]List<string> tags)
         {
-            var context = new TaskContext();
-
             var task = context.Task.FirstOrDefault(x => x.Id == id);
             if (task != null)
             {
+                task.Title = title;
+                task.Tags = CreateTagList.TagsList(tags);
                 task.Content = content;
                 task.LastModified = DateTime.Now.Date;
                 context.SaveChanges();
