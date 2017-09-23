4e2ef1ce26724094d5318cd36e9c13ce4ba491c6
Gleb <zadachings@gmail.com>
2017-09-23 23:22:26 +0300

    Update DeleteController.cs

--- TaskManager/Controllers/DeleteController.cs
+++ TaskManager/Controllers/DeleteController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +9,12 @@ namespace TaskManager.Controllers
 {
     public class DeleteController : Controller
     {
+        private TaskContext context;
+
+        public DeleteController()
+        {
+            context = new TaskContext();
+        }
         // GET: Delete
         public ActionResult Delete()
         {
@@ -18,8 +24,6 @@ namespace TaskManager.Controllers
         [HttpPost]
         public ActionResult Delete(int id)
         {
-            var context = new TaskContext();
-
             var task = context.Task.Find(id);
 
             if (task != null)
