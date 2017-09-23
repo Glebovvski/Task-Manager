f6369c006800d0b841a22ce304e2a0491c8aa1f7
Gleb <zadachings@gmail.com>
2017-09-23 23:23:57 +0300

    Update EditController.cs

--- TaskManager/Controllers/EditController.cs
+++ TaskManager/Controllers/EditController.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TaskManager.Helpers;
 using TaskManager.Models;
 
 namespace TaskManager.Controllers
@@ -10,25 +11,29 @@ namespace TaskManager.Controllers
     [Authorize]
     public class EditController : Controller
     {
-        // GET: Edit
+        private TaskContext context;
+
+        public EditController()
+        {
+            context = new TaskContext();
+        }
         
         // GET: Update/Edit/5
         public ActionResult Edit(int id)
         {
-            var context = new TaskContext();
             var task = context.Task.FirstOrDefault(x => x.Id == id);
             return View(task);
         }
 
         // POST: Update/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, string content)
+        public ActionResult Edit(int id, string content,string title,List<string> tags)
         {
-            var context = new TaskContext();
-
             var task = context.Task.FirstOrDefault(x => x.Id == id);
             if (task != null)
             {
+                task.Title = title;
+                task.Tags = CreateTagList.TagsList(tags);
                 task.Content = content;
                 task.LastModified = DateTime.Now.Date;
                 context.SaveChanges();
