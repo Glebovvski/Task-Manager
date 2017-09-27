991a355bfcf035b647b9e5e9d762dff1276b2601
Gleb <zadachings@gmail.com>
2017-09-27 20:16:27 +0300

    Update HomeController.cs

--- TaskManager/Controllers/HomeController.cs
+++ TaskManager/Controllers/HomeController.cs
@@ -22,15 +22,40 @@ namespace TaskManager.Controllers
 
         public ActionResult Index(int? page=1)
         {
+            List<string> types = new List<string>() { "Title", "Tags", "Content" };
+            SelectList typesList = new SelectList(types);
+            ViewBag.Types = typesList;
             int pageSize = 3;
             int pageNumber = (page ?? 1);
-            var tasks = context.Task.ToList();
+            var tasks = context.Task.OrderByDescending(x=>x.LastModified).ToList();
             return View(tasks.ToPagedList(pageNumber,pageSize));
+        }
+
+        public ActionResult Search(string search, string type)
+        {
+            var tasksearch = new List<PersonalTask>();
+            if (type == "Title")
+            {
+                tasksearch = context.Task.Where(s => s.Title.Contains(search)).ToList();
+            }
+            if (type == "Tags")
+            {
+                tasksearch = context.Task.Where(s => s.Tags.Contains(search)).ToList();
+            }
+            if (type == "Content")
+            {
+                tasksearch = context.Task.Where(s => s.Content.Contains(search)).ToList();
+            }
+            if (tasksearch.Count == 1)
+                return RedirectToAction("TaskView", new { id = tasksearch.FirstOrDefault().Id });
+            if (tasksearch.Count==0) throw new HttpException(404, "NotFound");
+            else return View(tasksearch);
+            
         }
 
         public ActionResult TaskView(int id)
         {
-            var task = context.Task.Find(id);
+            var task = context.Task.Where(x => x.Id == id).FirstOrDefault();
             return View(task);
         }
     }
