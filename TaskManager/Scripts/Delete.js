5e9c45994d0c4c46fc95435fd01a5ae0c7920037
Gleb <zadachings@gmail.com>
2017-09-29 18:52:21 +0300

    Add files via upload

---
+++ TaskManager/Scripts/Delete.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $.ajaxSetup({ cache: false });
+    $(".delItem").click(function (e) {
+
+        e.preventDefault();
+        $.get(this.href, function (data) {
+            $('#dialogContent').html(data);
+            $('#modDialog').modal('show');
+        });
+    });
+})
---
+++ TaskManager/Scripts/Search.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $('.link').click(function () {
+        var search = $("#search").val();
+        var type = $("#list option:selected").val();
+        this.href = this.href.replace("searchValue", search);
+        this.href = this.href.replace("typeValue", type);
+    });
+});
