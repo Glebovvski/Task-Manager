9778ea09cc0fd1db0455b93f9532912a66a9c23d
Gleb <zadachings@gmail.com>
2017-10-07 20:31:46 +0300

    Add files via upload

---
+++ TaskManager/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaskManager.Dto;
+using TaskManager.Models;
+
+namespace TaskManager.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<PersonalTask, PersonalTaskDto>();
+            Mapper.CreateMap<PersonalTaskDto, PersonalTask>();
+        }
+    }
+}
