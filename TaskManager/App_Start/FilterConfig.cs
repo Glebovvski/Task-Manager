db428b713696abb48dd16bf73a8cb23526951ecd
Gleb <zadachings@gmail.com>
2017-10-08 19:18:46 +0300

    Update FilterConfig.cs

--- TaskManager/App_Start/FilterConfig.cs
+++ TaskManager/App_Start/FilterConfig.cs
@@ -1,4 +1,4 @@
-﻿using System.Web;
+using System.Web;
 using System.Web.Mvc;
 
 namespace TaskManager
@@ -8,6 +8,7 @@ namespace TaskManager
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
