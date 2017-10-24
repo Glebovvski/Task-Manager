5e0fd0f096075aad6921d35b52f9c97789a85d56
Gleb <zadachings@gmail.com>
2017-10-24 17:57:18 +0300

    Update BundleConfig.cs

--- TaskManager/App_Start/BundleConfig.cs
+++ TaskManager/App_Start/BundleConfig.cs
@@ -11,8 +11,8 @@ namespace TaskManager
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/ToTop.js",
-                        "~/Scripts/Search.js",
-                        "~/Scripts/Delete.js"));
+                        "~/Scripts/delete.js",
+                        "~/Scripts/Search.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,6 +24,7 @@ namespace TaskManager
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
