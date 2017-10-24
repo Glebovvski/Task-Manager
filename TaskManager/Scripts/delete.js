8931df7111a0931bd9bcbac4bcc52fafab573c8c
Gleb <zadachings@gmail.com>
2017-10-24 17:55:21 +0300

    Add files via upload

---
+++ TaskManager/Scripts/delete.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(
+            (function () {
+                $.ajaxSetup({ cache: true });
+                $("#container").on("click", ".delItem", function (e) {
+                    var button = $(this);
+                    e.preventDefault();
+                    bootbox.confirm("Are you sure you want to delete this task?", function (result) {
+                        if (result) {
+                            $.ajax({
+                                url: "api/DeleteApi/" + button.attr("data-task-id"),
+                                method: "DELETE",
+                                success: function () {
+                                    button.parents("form").remove();
+                                    window.location.replace("https://localhost:44355/");
+                                }
+                            });
+                        }
+                    });  
+                });
+            }));
